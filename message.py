2c7e633b6577d87598c687d3860ac10011ebc8eb
MurkyMittens <murkymittens@mailmetrash.com>
2013-07-26 16:19:38 -0400

    added @staticmethod decorators to static message functions

--- message.py
+++ message.py
@@ -3,8 +3,10 @@ import json
 class Message(object):
 	SYSTEM_MESSAGE = 0
 
+	@staticmethod
 	def encode(type, payload):
 		return json.dumps({'type': type, 'payload': payload})
 
+	@staticmethod
 	def decode(message):
 		return json.loads(message)
