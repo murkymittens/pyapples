5772394977020dd43429051220bac239824e09df
MurkyMittens <murkymittens@mailmetrash.com>
2013-07-26 15:43:17 -0400

    making sure listenWS wasn't the culprit

--- server.py
+++ server.py
@@ -54,8 +54,8 @@ def main():
 	factory = Server("ws://{host}:{port}".format(host=host, port=port))
 	factory.protocol = ClientConnection
 	factory.setProtocolOptions(allowHixie76=True)
-	# listenWS(factory, interface=host)
-	listenWS(factory)
+	listenWS(factory, interface=host)
+	# listenWS(factory)
 	reactor.run()
 
 
